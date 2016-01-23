7aa756ad597723bbcf77d2aa5e56d59ecfbf1f19
Mateusz Rorat <rorat@azimo.com>
2016-01-23 21:51:27 +0100

    add heroku

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,7 @@
+'use strict';
 /* jshint devel:true */
 var elements = [];
 
-$(document).ready(function() {
-	elements['body'] = $('body');
-	elements['html'] = $('html');
-
-	if(UXPin.getLocation()) {
-		elements['html'].addClass('modal-open');
-	} 
-
-	$('.content-wrapper').click(function() {
-		UXPin.setLocation('profile');
-		elements['html'].addClass('modal-open');
-	});
-
-
-	$('.close-btn').click(function() {
-		UXPin.setLocation();
-		elements['html'].removeClass('modal-open');
-	});
-
-	UXPin.refreshView();
-
-})
-
-
-window.onhashchange = function() {
-	UXPin.setLocation(UXPin.getLocation());
-}
-// utils 
-
-function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
-}
 
 function UXPin () {}
 
@@ -40,21 +9,50 @@ UXPin.setLocation = function(location) {
 	$(this).addClass('active');
 	UXPin.resetView();
 	location = location ? '/#'+location : '/';
-	window.history.pushState("", "", location);
+	window.history.pushState('', '', location);
 	UXPin.refreshView();
-}
+};
 
 UXPin.getLocation = function() {
 	return window.location.hash.substring(1);
-}
+};
 
 UXPin.refreshView = function() {
-	$('[data-'+UXPin.getLocation()+']').addClass('active')
+	$('[data-'+UXPin.getLocation()+']').addClass('active');
 	if(!UXPin.getLocation()) {
-		elements['html'].removeClass('modal-open');
+		elements.html.removeClass('modal-open');
 	}
-}
+};
 
 UXPin.resetView = function() {
-	$('.active').removeClass('active')
-}
+	$('.active').removeClass('active');
+};
+
+$(document).ready(function() {
+	elements.body = $('body');
+	elements.html = $('html');
+
+	if(UXPin.getLocation()) {
+		elements.html.addClass('modal-open');
+	} 
+
+	$('.content-wrapper').click(function() {
+		UXPin.setLocation('profile');
+		elements.html.addClass('modal-open');
+	});
+
+
+	$('.close-btn').click(function() {
+		UXPin.setLocation();
+		elements.html.removeClass('modal-open');
+	});
+
+	UXPin.refreshView();
+
+});
+
+
+window.onhashchange = function() {
+	UXPin.setLocation(UXPin.getLocation());
+};
+
